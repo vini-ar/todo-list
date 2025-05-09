2f39b18f598313534db70cfaf3394075666980fe
Marcus Vinicyus <125088362+vini-ar@users.noreply.github.com>
2025-05-09 00:02:36 -0300

    Update Project

--- src/components/formValidations.js
+++ src/components/formValidations.js
@@ -1,9 +1,7 @@
 export function isValidForm() {
     const inputName = document.querySelector("#task-name-input")
-    const inputDescription = document.querySelector("#task-description-input")
 
     const name = inputName.value.trim()
-    const description = inputDescription.value.trim()
 
     if (!name) {
         console.error("Name Field Empty")
